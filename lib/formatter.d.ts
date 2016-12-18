a8ab4d8fb26cf5e530fbd8577f920535586c8c70
Gary Bortosky <garybort@gmail.com>
2016-12-17 23:52:35 -0600

    tinkering with formatter and ts 2.1

--- lib/formatter.d.ts
+++ lib/formatter.d.ts
@@ -2,8 +2,8 @@ export declare type FormatString = "EEE" | "MMM" | "MMMM" | "EEE MMM d, y h:mm a
 export declare class DateFormatter {
     private locale;
     private static zeros;
+    private static prependZero(num, cnt);
     constructor(locale?: string);
     format(date: Date | undefined, fmt: FormatString): string;
     private doFormat(parts, fmt);
-    private prependZero(num, cnt);
 }
--- src/formatter.ts
+++ src/formatter.ts
@@ -3,84 +3,85 @@
 import { DateParts } from "./dateparts";
 
 export type FormatString =
-    "EEE" | "MMM" | "MMMM" |
-    "EEE MMM d, y h:mm a" | "MMMM y";
+  "EEE" | "MMM" | "MMMM" |
+  "EEE MMM d, y h:mm a" | "MMMM y";
 
 type LongShort = [string, string];
 
 const monthNames: LongShort[] = [
-    ["January", "Jan"],
-    ["February", "Feb"],
-    ["March", "Mar"],
-    ["April", "Apr"],
-    ["May", "May"],
-    ["June", "Jun"],
-    ["July", "Jul"],
-    ["August", "Aug"],
-    ["September", "Sep"],
-    ["October", "Oct"],
-    ["November", "Nov"],
-    ["December", "Dec"],
+  ["January", "Jan"],
+  ["February", "Feb"],
+  ["March", "Mar"],
+  ["April", "Apr"],
+  ["May", "May"],
+  ["June", "Jun"],
+  ["July", "Jul"],
+  ["August", "Aug"],
+  ["September", "Sep"],
+  ["October", "Oct"],
+  ["November", "Nov"],
+  ["December", "Dec"],
 ];
 
 const dayNames: LongShort[] = [
-    ["Sunday", "Sun"],
-    ["Monday", "Mon"],
-    ["Tuesday", "Tue"],
-    ["Wednesday", "Wed"],
-    ["Thursday", "Thu"],
-    ["Friday", "Fri"],
-    ["Saturday", "Sat"],
+  ["Sunday", "Sun"],
+  ["Monday", "Mon"],
+  ["Tuesday", "Tue"],
+  ["Wednesday", "Wed"],
+  ["Thursday", "Thu"],
+  ["Friday", "Fri"],
+  ["Saturday", "Sat"],
 ];
 
 export class DateFormatter {
-    private static zeros: string = Array(5).join("0");
+  private static zeros: string = Array(5).join("0");
 
-    constructor(private locale: string = "en-US") {
-        if (locale !== "en-US") {
-            throw new RangeError("Only en-US is supported.");
-        }
+  private static prependZero(num: number, cnt: number): string {
+    return DateFormatter.zeros.concat(num.toString(10)).slice(-cnt);
+  }
+
+  constructor(private locale: string = "en-US") {
+    if (locale !== "en-US") {
+      throw new RangeError("Only en-US is supported.");
+    }
+  }
+
+  public format(date: Date | undefined, fmt: FormatString): string {
+    if (!date) {
+      throw new RangeError("Date is required.");
+    }
+    return this.doFormat(new DateParts(date), fmt);
+  }
+
+  private doFormat(parts: DateParts, fmt: FormatString): string {
+    switch (fmt) {
+      case "EEE": {
+        return dayNames[parts.w][0];
+      }
+      case "MMM": {
+        return monthNames[parts.m][1];
+      }
+      case "MMMM": {
+        return monthNames[parts.m][0];
+      }
+      case "MMMM y": {
+        return `${this.doFormat(parts, "MMMM")} ${parts.y.toFixed(0)}`;
+      }
+      case "EEE MMM d, y h:mm a": {
+        const ps = [
+          `${this.doFormat(parts, "EEE")}`,
+          `${this.doFormat(parts, "MMM")}`,
+          `${parts.d}, ${parts.y}`,
+          `${parts.h}:${DateFormatter.prependZero(parts.mm, 2)}`,
+          `${parts.a}`,
+        ];
+        return ps.join(" ");
+      }
+      default: {
+        throw new Error(`The fmt value '${fmt}' is not supported.`);
+      }
     }
 
-    public format(date: Date | undefined, fmt: FormatString): string {
-        if (!date) {
-            throw new RangeError("Date is required.");
-        }
-        return this.doFormat(new DateParts(date), fmt);
-    }
+  }
 
-    private doFormat(parts: DateParts, fmt: FormatString): string {
-        switch (fmt) {
-            case "EEE": {
-                return dayNames[parts.w][0];
-            }
-            case "MMM": {
-                return monthNames[parts.m][1];
-            }
-            case "MMMM": {
-                return monthNames[parts.m][0];
-            }
-            case "MMMM y": {
-                return `${this.doFormat(parts, "MMMM")} ${parts.y.toFixed(0)}`;
-            }
-            case "EEE MMM d, y h:mm a": {
-                const ps = [
-                    `${this.doFormat(parts, "EEE")}`,
-                    `${this.doFormat(parts, "MMM")}`,
-                    `${parts.d}, ${parts.y}`,
-                    `${parts.h}:${this.prependZero(parts.mm, 2)}`,
-                    `${parts.a}`,
-                ];
-                return ps.join(" ");
-            }
-            default: {
-                throw new Error(`The fmt value '${fmt}' is not supported.`);
-            }
-        }
-
-    }
-
-    private prependZero(num: number, cnt: number): string {
-        return DateFormatter.zeros.concat(num.toString(10)).slice(-cnt);
-    }
 }
